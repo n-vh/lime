a3a56e98d3815d3feb66c8c6b701a99373895c59
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-02-21 15:27:44 +0100

    change index css and html

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Index } from '~/pages/Index';
+import { Index } from '~/pages/LandingPage';
 import { Navbar } from '~/components/Navbar';
 import { Card } from '~/components/Card';
 import { Sign } from '~/pages/Sign';
