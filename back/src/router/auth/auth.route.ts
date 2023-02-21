528e560ebbe85f17501c7290033cd079dc090870
Nils <nils.vanhee@pm.me>
2023-02-21 13:58:18 +0100

    add signup/verify route

--- back/src/router/auth/auth.route.ts
+++ back/src/router/auth/auth.route.ts
@@ -1,11 +1,16 @@
 import type { FastifyPluginCallback, FastifyRequest } from 'fastify';
 import type { IUser } from '~/shared/types';
-import { UserController } from '~/controllers';
+import { MailVerifyController, UserController } from '~/controllers';
 import { SignInRouteSchema, SignUpRouteSchema } from './auth.schema';
 import { comparePassword, hashPassword } from '~/utils/password';
+import { MailVerifyType } from '~/shared/enums';
 
 type SignUpRouteRequest = FastifyRequest<{
   Body: Pick<IUser, 'username' | 'email' | 'password'>;
+}>;
+
+type SignUpRouteVerifyRequest = FastifyRequest<{
+  Body: { token: string };
 }>;
 
 type SignInRouteRequest = FastifyRequest<{
@@ -28,6 +33,38 @@ export const authRouter: FastifyPluginCallback = (app, opts, next) => {
         // when the user is created, send a Bearer token
         rep.send({
           token: app.jwt.sign({ payload: user.toJSON() }, { expiresIn: '7d' }),
+
+  app.route({
+    url: '/signup/verify',
+    method: 'POST',
+    handler: async (req: SignUpRouteVerifyRequest, rep) => {
+      try {
+        // decode the token
+        const token = app.jwt.verify(req.body.token);
+        const user = token['payload'];
+
+        // check if token is expired
+        const dateNow = Math.floor(Date.now() / 1000);
+        if (dateNow >= token['exp']) {
+          throw new Error('TOKEN_EXPIRED');
+        }
+
+        // delete the mail verify token
+        // throws error if token is not found
+        await MailVerifyController.deleteOne({
+          token: req.body.token,
+        });
+
+        // create the user
+        // throws error if user is not found
+        await UserController.create({
+          ...user,
+          password: await hashPassword(user.password),
+        });
+
+        rep.send({
+          status: 200,
+          message: 'SIGNUP_SUCCESS',
         });
       } catch (e) {
         rep.status(400).send({
