aaf91635b7828cd36703c5ba2bcc4830d971e071
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-02-22 13:57:39 +0100

    page show all

---
+++ front/src/components/DividerComp.tsx
@@ -0,0 +1,10 @@
+export function Divider() {
+  return (
+    <div>
+      <button className="h-10 w-40 translate-y-[1.3rem] rounded-full border-2 border-[#E3FF7D] bg-[#27272a] md:h-10 md:w-64 md:translate-y-[1.6rem] lg:h-14 lg:w-80 lg:translate-y-[2rem]">
+        <h1 className="text-bold text-white"> A</h1>
+      </button>
+      <hr className=" border border-[#E3FF7D]" />
+    </div>
+  );
+}
--- front/src/pages/PageShowAll.tsx
+++ front/src/pages/PageShowAll.tsx
@@ -1,94 +1,26 @@
+import { ButtonTypes } from '~/components/ButtonTypes';
+import { Carousel } from '~/components/CarouselComp';
+import { Divider } from '~/components/DividerComp';
 import { Navbar } from '~/components/Navbar';
 
 export function PageShowAll() {
   return (
     <>
       <Navbar />
-      <div className="absolute top-0 z-0 h-2/6 w-full bg-gray-200">
-        <div className="right-0 left-0 h-2/6 rounded-lg "></div>
-      </div>
-      <div className="mt-[285px] mr-8 grid grid-cols-2 gap-8 pl-8 pr-8 pt-8 md:grid-cols-3 lg:grid-cols-4">
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
-        </div>
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
-        </div>
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
-        </div>
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
-        </div>
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
-        </div>
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
-        </div>
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
-        </div>
-        <div className="relative overflow-hidden">
-          <img
-            src="./images/placeholder.png"
-            alt="your-image-alt"
-            className="h-auto w-full"
-          />
-          <div className="absolute inset-0 bg-gray-800 bg-opacity-50 opacity-0 backdrop-blur-md backdrop-filter transition duration-500 hover:opacity-100">
-            {/* any content that should appear on top of the image, e.g. a caption */}
-          </div>
+      <div className="absolute top-0 z-0 h-2/6 w-full rounded-b-2xl bg-gray-200">
+        <div className="right-0 left-0 h-3/6 rounded-lg ">
+          <div className="h4/6 w-full bg-slate-300"></div>
         </div>
       </div>
+
+      <div className="ml-4 mt-[285px] md:mt-[395px] lg:mt-[365px]"></div>
+
+      <Divider />
+
+      <Carousel />
+      <Divider />
+
+      <Carousel />
     </>
   );
 }
