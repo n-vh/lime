35a1326472908361c84e49797745e9b073db8cfa
edgarleon93 <119509538+edgarleon93@users.noreply.github.com>
2023-02-23 10:06:06 +0100

    change filter color

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Index } from '~/pages/LandingPage';
+import { Index } from '~/pages/Index';
 import { Navbar } from '~/components/Navbar';
 import { Card } from '~/components/Card';
 import { SignIn } from '~/pages/SignIn';
